c52ac66c6bcad7b971d626eb113ff8f0022b7b2e
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-13 15:05:55 +0100

    Add secure SECRET_KEY to environment configuration

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r0nrfam4rjw3%f6ak7seg+do70^pc5r(r@-jlhfv^##q)11u)0'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
-
+ 
 
 # Application definition
 
