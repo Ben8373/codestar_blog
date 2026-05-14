71e19a79bbd822596d71bf32e4cc112d3a15aca2
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-14 21:10:27 +0100

    Fix: Switch to WhiteNoiseStaticFilesStorage

--- codestar/settings.py
+++ codestar/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# WhiteNoise compression and caching
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# WhiteNoise static files storage
+STATICFILES_STORAGE = "whitenoise.storage.WhiteNoiseStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
