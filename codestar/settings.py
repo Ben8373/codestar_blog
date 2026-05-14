69f00345fd3b5e687b449d83d4fcbf4523b93dc1
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-14 12:48:31 +0100

    disable DEBUG for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,16 +34,7 @@ ALLOWED_HOSTS = [
     "benjamin-codestarblog-76259bbfc8cf.herokuapp.com",
     "localhost",
     "127.0.0.1",
-]
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
+] 
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
