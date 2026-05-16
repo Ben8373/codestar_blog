1d417369dc97662f602dfd11aeb3455221aa4fa1
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-16 17:10:20 +0100

    fix broken admin.py and comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ('status', 'created_on') 
+    list_filter = ('status', 'created_on')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
