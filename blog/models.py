b44d07b042c30cb2c3a1769795ed948b5bb22c76
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-16 15:33:45 +0100

    add Comment model and preform migrations

--- blog/models.py
+++ blog/models.py
@@ -16,9 +16,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
-class Comments(models.Model):
-    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
