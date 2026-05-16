057407666ade7079fb2718515830f1d4bbe3a2b4
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-16 14:49:32 +0100

    create comments model in models.py

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,16 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Comments(models.Model):
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add_=True)
