f614f20e562c12e182e0cc708aab1dd1bae32682
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-17 13:52:37 +0100

    clean up admin.py and fix CommentAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', 'created_on')
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
-    
+
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created_on')
-    list_filter = ('created_on',)
-    search_fields = ('name', 'email', 'body')
+    list_display = ('author', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author__username', 'body')
