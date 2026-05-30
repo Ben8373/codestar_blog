332ddef421a94d53f542c2b1cb1ada922dac2f10
Ben8373 <103134414+Ben8373@users.noreply.github.com>
2026-05-30 20:56:33 +0100

    added authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("about/", include("aboutapp.urls")),
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("blog/", include("blog.urls")),
